c5314bc9b49d3baa2568a951a33283a9ac4fac06
Amir HN ( Data0Face ) <TheData0Face@gmail.com>
2018-05-24 17:31:32 +0430

    Add files via upload

---
+++ BasicStorePage/Store/insertcustomer.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style type="text/css">
+#t1{ width:200px; height:100px; background:blue; position:fixed; top:0}
+#t2{ width:200px; height:100px; background:green; position:fixed; top:0}
+</style>
+</head>
+
+<body>
+<div id="t1"></div>
+<div id="t2"></div>
+</body>
+</html>
